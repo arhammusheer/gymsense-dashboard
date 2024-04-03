0bc3879e1b14424c40ee1166d2d84e84504ed742
arhammusheer <arhammusheer@gmail.com>
2024-04-03 08:32:54 -0400

    Removed useauth

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/bebas-neue";
 import "@fontsource/bungee";
 import theme from "./theme.ts";
-import { AuthProvider } from "./hooks/useAuth.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <AuthProvider>
-        <ChakraProvider theme={theme}>
-          <App />
-        </ChakraProvider>
-      </AuthProvider>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>
 );
