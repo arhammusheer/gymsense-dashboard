6c28c9eadb15edbfc1ffa2244e1fe24b6d7b00db
arhammusheer <arhammusheer@gmail.com>
2024-04-08 05:01:11 -0400

    SDP API change

--- src/redux/const.ts
+++ src/redux/const.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "https://gymsense-api-production.up.railway.app";
+// export const API_BASE_URL = "https://gymsense-api-production.up.railway.app";
+export const API_BASE_URL = "https://api.sdp.croissant.one";
